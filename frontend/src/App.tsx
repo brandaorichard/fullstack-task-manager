ad6a75f8d20485646d0d75a496ba7f9b191f1df1
brandaorichard <rih.brandao@gmail.com>
2025-03-06 21:32:40 -0300

    front-end initial environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, HStack } from "@chakra-ui/react"
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </div>
+  )
+}
+
+export default App
